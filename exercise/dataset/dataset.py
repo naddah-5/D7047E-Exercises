9430a41f1934d7ea9a5011e6bcd468f67f84de9c
naddah-5 <andersoskardahl@hotmail.com>
2023-04-03 09:34:19 +0200

    wip

---
+++ exercise/dataset/dataset.py
@@ -0,0 +1,20 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+def load_dataset(batch_size: int = 10):
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    test_val_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    valset , testset = torch.utils.data.random_split(test_val_set, [0.4, 0.6])
+
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+    valloader = torch.utils.data.DataLoader(valset, batch_size=batch_size, shuffle=False, num_workers=2)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+
+    labels = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    return trainloader, valloader, testloader, labels
