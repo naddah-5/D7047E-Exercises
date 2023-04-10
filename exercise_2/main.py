29412a8141db04b7448168320cd6e41ce356851c
naddah-5 <andersoskardahl@hotmail.com>
2023-04-10 14:26:10 +0200

    wip Co-authored-by: CSaturnus <CSaturnus@users.noreply.github.com> Co-authored-by: Kevand98 <Kevand98@users.noreply.github.com>

--- exercise_2/main.py
+++ exercise_2/main.py
@@ -1,17 +1,19 @@
 from dataset.dataset import Dataset
-from model.feature_extract import Fea
+from model.transfer_learning import transfer_model
 from model.alex import Alex
 from model.cnn import CNN
 from training.training import Training
 import torch
 
 def main():
-    epochs = 200
+    epochs = 1
     batch_size = 10
     learning_rate = 0.0001
     best_net: str = ''
 
     my_device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # set device to gpu if available
+    print("using device ", my_device)
+
 
     datasetMNIST = Dataset(batch_size, MNIST=True)
     datasetSVHN = Dataset(batch_size, SVHN=True)
@@ -19,10 +21,13 @@ def main():
     train_loader_MNIST, val_loader_MNIST, test_loader_MNIST, _ = datasetMNIST.load_dataset()
     train_loader_SVHN, val_loader_SVHN, test_loader_SVHN, _ = datasetSVHN.load_dataset()
 
-    model = 
+    model = CNN(class_count=10, device=my_device)
+    model.to(my_device)
 
     training = Training(model, train_loader_MNIST, val_loader_MNIST, test_loader_MNIST, test_loader_SVHN, epochs, learning_rate, device=my_device)
     training.train_model()
+    
+    #model = transfer_model().CNN(network_path="best_network.pt")
 
     best_accuracy_MNIST = training.test_model_MNIST()
     best_accuracy_SVHN = training.test_model_SVHN()
--- exercise_2/model/transfer_learning.py
+++ exercise_2/model/transfer_learning.py
@@ -2,10 +2,14 @@ from model.cnn import CNN
 import torch
 import torch.nn as nn
 
-class FeatureExtractCNN():
-    def __init__(self, network: str = "", device: str = "cpu", in_count: int = 21, class_count: int = 10) -> None:
+class transfer_model():
+    def __init__(self) -> None:
+        self.network = None
+    
+    
+    def CNN(self, network_path: str = "", device: str = "cpu", in_count: int = 21, class_count: int = 10) -> None:
         network = CNN(device=device)
-        network = torch.load(network)
+        network = torch.load(network_path)
         for parameter in network.parameters():
             parameter.requires_grad = False
         network.classifier[-1].requires_grad = True
